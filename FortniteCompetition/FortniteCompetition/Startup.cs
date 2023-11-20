02f15de917bc5f9173c3658d548a7f06cb6191d9
nsbhalli <2011615@studentmail.ucol.ac.nz>
2023-11-21 09:37:52 +1300

    Login page and Register page working correctly and implemented

--- FortniteCompetition/FortniteCompetition/Startup.cs
+++ FortniteCompetition/FortniteCompetition/Startup.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using FortniteCompetition.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,14 +17,35 @@ namespace FortniteCompetition
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+
+            services.AddDbContext<IdentityDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("IdentityDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<IdentityDataContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseIdentity();
+
             app.UseMvc(routes => {
                 routes.MapRoute("Default",
                     "{controller=Home}/{action=Index}/{id?}"
